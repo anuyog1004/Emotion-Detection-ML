43221d3e229fd6aa47232e09c881debcda17eaf1
anuyog1004 <anuyog1004@gmail.com>
2017-09-05 20:47:43 +0530

    Fixing variable names

--- organise_dataset.py
+++ organise_dataset.py
@@ -12,42 +12,48 @@ import logging
 from shutil import copyfile
 
 if __name__ == '__main__':
+
     # Ordering defined in the readme downloaded form CK+ dataset.
     emotions = ["neutral", "anger", "contempt", "disgust", "fear", "happy", "sadness",
                 "surprise"]
 
     # List for all file names in the folder "source_emotion".
-    participants = glob.glob("source_emotion/*")
+    source_emotion_folders = glob.glob("source_emotion/*")
 
     # Copy all images from "source_images" to "sorted_set".
-    for x in participants:
-        part = "%s" % x[-4:]
-        for sessions in glob.glob("%s/*" % x):
-            for files in glob.glob("%s/*" % sessions):
-                current_session = files[20:-30]
-                file = open(files, 'r')
+    for folder in source_emotion_folders:
+        folder_number = "%s" % folder[-4:]
+        for file_names in glob.glob("%s/*" % folder):
+            for file_name in glob.glob("%s/*" % file_names):
+                current_file = file_name[20:-30]
+                emotion_label_file = open(file_name, 'r')
 
-                emotion = int(float(file.readline()))
+                with open(file_name, 'r') as emotion_label_file:
+                    emotion = int(float(emotion_label_file.readline()))
 
-                sourcefile_emotion = glob.glob("source_images/%s/%s/*" % (part, current_session))[-1]
-                sourcefile_neutral = glob.glob("source_images/%s/%s/*" % (part, current_session))[0]
+                    sourcefile_emotion = glob.glob(
+                        "source_images/%s/%s/*" % (folder_number, current_file))[-1]
+                    sourcefile_neutral = glob.glob(
+                        "source_images/%s/%s/*" % (folder_number, current_file))[0]
 
-                dest_neut = "sorted_set/neutral/%s" % sourcefile_neutral[25:]
-                dest_emot = "sorted_set/%s/%s" % (emotions[emotion], sourcefile_emotion[25:])
+                    destination_neutral = "sorted_set/neutral/%s" % sourcefile_neutral[
+                        25:]
+                    destination_emotion = "sorted_set/%s/%s" % (
+                        emotions[emotion], sourcefile_emotion[25:])
 
-                copyfile(sourcefile_neutral, dest_neut)
-                copyfile(sourcefile_emotion, dest_emot)
+                    copyfile(sourcefile_neutral, destination_neutral)
+                    copyfile(sourcefile_emotion, destination_emotion)
 
     # Used for cropping out the face from the image.
     face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
     # Crop face from each image in "sorted_set" and copy to "dataset".
     for emotion in emotions:
-        files = glob.glob("sorted_set/%s/*" % emotion)
+        file_names = glob.glob("sorted_set/%s/*" % emotion)
         file_number = 0
 
-        for f in files:
-            img = cv2.imread(f)
+        for file_name in file_names:
+            img = cv2.imread(file_name)
             gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
             faces = face_cascade.detectMultiScale(gray, 1.3, 5)
             for (x, y, w, h) in faces:
@@ -55,7 +61,8 @@ if __name__ == '__main__':
 
                 try:
                     new_image = cv2.resize(gray, (300, 350))
-                    cv2.imwrite("dataset/%s/%s.jpg" % (emotion, file_number), new_image)
+                    cv2.imwrite("dataset/%s/%s.jpg" %
+                                (emotion, file_number), new_image)
                 except cv2.error:
                     logging.warning(cv2.error.message)
 
